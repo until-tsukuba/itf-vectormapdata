91990d4e3b06256a48090b9c1788ddda048d8a2d
oageo <59683387+oageo@users.noreply.github.com>
2025-07-14 20:14:47 +0900

    UIを調整

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import tailwindcss from "@tailwindcss/vite";
 import purgecss from 'astro-purgecss';
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
   integrations: [purgecss()]
 });
